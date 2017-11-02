38919b41a1a1821a192d0a5fa558f90334104181
invisiblesushi <danielcoco96@gmail.com>
2017-11-02 17:42:15 +0100

    t

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -48,7 +48,7 @@ public class HelloWorld {
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
 		// Create the window
-		window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+		window = glfwCreateWindow(1280, 720, "Hello World!", NULL, NULL);
 		if ( window == NULL )
 			throw new RuntimeException("Failed to create the GLFW window");
 
